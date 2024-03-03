db72cc9a213549bc257507a67826ec4ad524c0ed
NunoMars <nuno.ricardo.mars@gmail.com>
2024-03-03 23:52:24 +0100

    last config

--- app/siteVoyanceconfig/settings/__init__.py
+++ app/siteVoyanceconfig/settings/__init__.py
@@ -109,8 +109,8 @@ WSGI_APPLICATION = "siteVoyanceconfig.wsgi.application"
 
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+DB_USER = os.environ.get("POSTGRES_USER")
 
 
 DATABASES = (
