c93b1435ad5529cba7fb73bd168f0743c7eb4311
NunoMars <nuno.ricardo.mars@gmail.com>
2024-03-04 22:37:37 +0100

    deplace

---
+++ app/accounts/management/commands/create_superuser.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = "Create a superuser"
+
+    def handle(self, *args, **kwargs):
+        # Créer un super utilisateur
+        User = get_user_model()
+        try:
+            superuser = User.objects.create_superuser(
+                "loupy2222@hotmail.com", "Bcxau9p^^123."
+            )
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Superuser created, email: {superuser.email}, password: Bcxau9p^^123."
+                )
+            )
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f"Superuser not created, error: {e}"))
