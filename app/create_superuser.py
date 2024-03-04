f135efb4d2caf5973cab38bb518050a1cd49f485
NunoMars <nuno.ricardo.mars@gmail.com>
2024-03-04 22:03:41 +0100

    trylast

--- app/create_superuser.py
+++ app/create_superuser.py
@@ -1,12 +1,12 @@
-# creer super user
 import os
+import django
+from accounts.models import CustomUser
 
+# creer super user
 PASSWORD = os.environ.get("DB_PASSWORD")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "siteVoyanceconfig.settings")
-import django
 
 django.setup()
-from accounts.models import CustomUser
 
 try:
     CustomUser.objects.create_superuser(
@@ -16,3 +16,4 @@ try:
     print(f"Superuser created, email:loupy2222@hotmail.com, password:Bcxau9p^^123.")
 except Exception as e:
     print(f"Superuser not created, error: {e}")
+    pass
